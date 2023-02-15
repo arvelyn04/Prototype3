5ce454a2be83db8d3975d1020753df853866a5ca
arvelyn04 <arvelynaramos@iskolarngbayan.pup.edu.ph>
2023-02-15 23:09:04 +0800

    Initial Commit
    .gitignore
    .gitattributes

---
+++ Prototype3/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private float speed = 30;
+    private PlayeController playeControllerScript;
+    private float leftBound = -15;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playeControllerScript = GameObject.Find("Player").GetComponent<PlayeController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playeControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
---
+++ Prototype3/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private float startDelay = 2;
+    private float repeaRate = 2;
+    private PlayeController playeControllerScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playeControllerScript = GameObject.Find("Player").GetComponent<PlayeController>();
+        InvokeRepeating("SpawnObstacle", startDelay, repeaRate);    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle ()
+    {
+        if(playeControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+    }
+}
